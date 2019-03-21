990a87dd15b6086e2b2454b8f134d9c7f6dfb5f5
nhunguyen997 <48782220+nhunguyen997@users.noreply.github.com>
2019-03-21 12:41:31 +0700

    Update sapxep.py

--- sapxep.py
+++ sapxep.py
@@ -1,8 +1,12 @@
-def sapxep(s):
+def sapxep():
+    print("nhap vao mot chuoi:")
+    s = input()
+    print("ban da nhap vao: \n", s)
     s = s.split()
     s = list(set(s))
     s.sort()
+    print("sau khi sap xep:")
     print(s)
 
 
-sapxep('nguyen thi nhu nhu')
+sapxep()
